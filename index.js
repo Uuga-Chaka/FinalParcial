acc0af5f2a1f91ae1ed57c71b718dcc6ad9f4930
Juan Pablo <jpgarias09@gmail.com>
2018-05-25 11:54:51 -0500

    Todo terminado con stilos y que tales

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
 const express = require('express'),
     consolidate = require('consolidate'),
-    app = express();
+    app = express(),
+    fs = require('fs');
 
 
 app.engine('hbs', consolidate.handlebars);
 app.set('views', './views');
 app.set('view engine', 'hbs');
 
+app.use(express.static("public"));
+
+var inicio = 0;
+var producto = 0;
+var contacto = 0;
+
 app.get("/", (req, res) => {
-    res.render("index");
+    inicio += 1;
+    fs.writeFileSync("logs.txt", "Inicio: " + inicio+"\n"+"Producto: " + producto+"\n"+"Contacto: " + contacto);
+    res.render("index", {
+        titulo: "Inicio"
+    });
 });
 
 app.get("/producto", (req, res) => {
-    res.render("producto");
+    producto += 1;
+    fs.writeFileSync("logs.txt", "Inicio: " + inicio+"\n"+"Producto: " + producto+"\n"+"Contacto: " + contacto);
+    res.render("producto", {
+        titulo: "Producto"
+    });
 });
 
 app.get("/contacto", (req, res) => {
-    app.render("contacto");
+    contacto += 1;
+    fs.writeFileSync("logs.txt", "Inicio: " + inicio+"\n"+"Producto: " + producto+"\n"+"Contacto: " + contacto);
+    res.render("contacto", {
+        titulo: "Contacto"
+    });
 });
 
 app.listen(1234, () => {
